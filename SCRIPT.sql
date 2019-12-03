b9890d136be59b796dc5e1422a3798f6f1451acd
Mauricio Gomez <mauricio_gomezrod@hotmail.com>
2019-12-03 11:45:51 -0500

    Add files via upload

---
+++ SCRIPT.sql
@@ -0,0 +1,32 @@
+-- Create table
+create table USUARIOS
+(
+  login    VARCHAR2(20) not null,
+  password VARCHAR2(10)
+)
+tablespace USERS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
+-- Create/Recreate primary, unique and foreign key constraints 
+alter table USUARIOS
+  add constraint USUARIOS_PK primary key (LOGIN)
+  using index 
+  tablespace USERS
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
